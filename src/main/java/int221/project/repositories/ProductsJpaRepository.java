a49490cc64bec5b00ddc2c7e51651ddabf7f320e
punthanat <punthanat@hotmail.com>
2021-05-05 22:50:47 +0700

    [fix] exception name v0.2

--- src/main/java/int221/project/repositories/ProductsJpaRepository.java
+++ src/main/java/int221/project/repositories/ProductsJpaRepository.java
@@ -1,11 +1,9 @@
 package int221.project.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import int221.project.models.Product;
 
 public interface ProductsJpaRepository extends JpaRepository<Product, Integer> {
-	List<Product> findByName(String name);
+	Product findByName(String name);
 }
